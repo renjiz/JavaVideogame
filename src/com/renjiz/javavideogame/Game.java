da552440812999c6ccb627369d85dc5e884f4ecf
renjiz <renji_1308@hotmail.com>
2013-10-27 13:29:47 -0600

    Video 6 Inicializando graficos, haciendo el fondo negro.

--- src/com/renjiz/javavideogame/Game.java
+++ src/com/renjiz/javavideogame/Game.java
@@ -1,7 +1,9 @@
 package com.renjiz.javavideogame;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
@@ -55,10 +57,16 @@ public class Game extends Canvas implements Runnable {
 
 	public void render() {
 		BufferStrategy bs = getBufferStrategy();
-		if (bs == null){
+		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.dispose();
+		bs.show();
 
 	}
 
