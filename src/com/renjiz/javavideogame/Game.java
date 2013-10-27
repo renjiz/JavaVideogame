715a7b60d22942fc586d13ca736f15d8ecdac374
renjiz <renji_1308@hotmail.com>
2013-10-27 01:16:07 -0600

    Video 2

--- src/com/renjiz/javavideogame/Game.java
+++ src/com/renjiz/javavideogame/Game.java
@@ -1,8 +1,24 @@
 package com.renjiz.javavideogame;
 
-public class Game {
+public class Game implements Runnable {
 
 	public static int width = 300;
-	public static int height = width /16*9;
+	public static int height = width / 16 * 9;
 	public static int scale = 3;
+
+	private Thread thread;
+
+	public synchronized void start() {
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
